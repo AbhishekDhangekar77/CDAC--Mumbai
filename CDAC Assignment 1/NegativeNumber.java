825ecf6ae89db183dfa2124f29c052535e713983
Abhishek Dhangekar <abhidhangekar@gmail.com>
2024-08-24 16:06:14 +0530

    Add files via upload

---
+++ CDAC Assignment 1/NegativeNumber.java
@@ -0,0 +1,11 @@
+class NegativeNumber {
+public static void main(String[] args){
+int number = -5;
+if(number < 0){
+System.out.println("This number is negative.");
+}
+else{
+System.out.println("This number is not negative.");
+}
+}
+}
---
+++ CDAC Assignment 1/OddorEven.java
@@ -0,0 +1,10 @@
+ public class OddorEven  {
+ public static void main(String[] args){
+int number = 6;
+if (number %2==0){
+System.out.println("This number is even.");
+} else {
+System.out.println("This number is odd.");
+}
+}
+}
